93d0fdaca63516cf0499a6b978c774a5f173c7d3
David N <dtndccp2020@outlook.com>
2020-06-24 16:12:02 -0400

    Added a comments

---
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' First comment when form loads
+        ' lalalalalall 
+        ' Hello Hello
+    End Sub
+End Class
